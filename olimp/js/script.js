ab23ce7350557b69679cd13e6ace0b003405f282
fogfug1 <fogfug1@.gmail.com>
2018-07-23 22:54:30 +0300

    reg olimp v1

--- olimp/js/script.js
+++ olimp/js/script.js
@@ -1,8 +1,3 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
-// function SetResizeContent() {
-// 	var minheight = $(window).height();
-// 	$("body").css('min-height', minheight);
-// };
-//  SetResizeContent();
