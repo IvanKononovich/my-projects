e0a60b8e06dbd0e2c34bec810c16c8134d11ad04
fogfug1 <fogfug1@.gmail.com>
2018-07-23 19:48:00 +0300

    Fixed bag index olimp

--- olimp/js/script.js
+++ olimp/js/script.js
@@ -1,4 +1,8 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
+function SetResizeContent() {
+	var minheight = $(window).height();
+	$("body").css('min-height', minheight);
+};
+ SetResizeContent();
