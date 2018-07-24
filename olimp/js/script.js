f599700e54c885068c5b5e3da089d9511923725a
fogfug1 <fogfug1@.gmail.com>
2018-07-24 16:07:08 +0300

    info olimp v1

--- olimp/js/script.js
+++ olimp/js/script.js
@@ -1,8 +1,9 @@
 particlesJS.load('particles-js', 'assets/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
-function SetResizeContent() {
-	var maxWidth = $(window).width();
-	$("canvas").css('max-width', maxWidth);
-	console.log(maxWidth);
-};
+// function SetResizeContent() {
+// 	var minheight = $(".particles").height();
+// 	$("canvas").css('min-height', minheight);
+// 	console.log(minheight);
+// };
+// SetResizeContent();
