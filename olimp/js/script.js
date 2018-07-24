dd24a3d59e28836bf101662999cbb57d15be5bd3
fogfug1 <fogfug1@.gmail.com>
2018-07-24 18:52:23 +0300

    full olimp

--- olimp/js/script.js
+++ olimp/js/script.js
@@ -1,9 +1,7 @@
-particlesJS.load('particles-js', 'assets/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
-// function SetResizeContent() {
-// 	var minheight = $(".particles").height();
-// 	$("canvas").css('min-height', minheight);
-// 	console.log(minheight);
-// };
-// SetResizeContent();
+function SetResizeContent() {
+	var minwidth = $("main").width();
+	$(".wrapper").css('min-width', minwidth + 160);
+};
+SetResizeContent();
+
+
