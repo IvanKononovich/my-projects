abbd83635b4831bd7c3847d1a1644a83cd385225
fogfug1 <fogfug1@.gmail.com>
2018-07-24 19:07:00 +0300

    full v2

--- olimp/js/script.js
+++ olimp/js/script.js
@@ -1,6 +1,8 @@
 function SetResizeContent() {
 	var minwidth = $("main").width();
 	$(".wrapper").css('min-width', minwidth + 160);
+	var minheight = $("main").height();
+	$(".wrapper").css('min-height', minheight);
 };
 SetResizeContent();
 
